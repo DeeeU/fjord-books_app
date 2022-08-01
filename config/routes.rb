c1533f4e2c55c7c9250622bafda7cb6d87319bd4
Y.Kanabuchi <yutaro.kanabuchi@medpeer.co.jp>
2022-08-01 09:50:04 +0900

    add Mailer 今回メールでのパスワード変更をする。新しく今回は'letter_opener'を導入したこれによりメール送信の処理を簡単に作れる。 https://railsguides.jp/action_mailer_basics.html また、メール送信機能の実装のために宛先や送り主の設定ができるファイル'app/mailers/application_mailer.rb'などを作成した。

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {   registrations: 'users/registrations',
     sessions: 'users/sessions' }
   resources :books
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
 end
