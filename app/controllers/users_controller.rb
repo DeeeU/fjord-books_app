83a44074988ef6806cef22dba8694ee338097ef3
Y.Kanabuchi <yutaro.kanabuchi@medpeer.co.jp>
2022-08-05 12:01:38 +0900

    add: 詳細ページの追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,8 @@ class UsersController < ApplicationController
   def index
     @users = User.order(:id).page(params[:page]).per(5)
   end
+
+  def show
+    @user = User.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root 'books#index'
   devise_for :users, controllers: { registrations: 'users/registrations',
     sessions: 'users/sessions' }
-  resources :users, :only => [:index]
+  resources :users, :only => [:index,:show]
   resources :books
 
   if Rails.env.development?
