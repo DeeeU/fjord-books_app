52f6fe190cfc6f94d2a025e7faa87a8564ae3139
Y.Kanabuchi <yutaro.kanabuchi@medpeer.co.jp>
2022-08-05 09:15:15 +0900

    change: 一覧ページへのパス追加 一覧ページのパス追加とその中を追加した。 中身の部分はまだ仮の状態

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/index'
   root 'books#index'
-  devise_for :users, controllers: {   registrations: 'users/registrations',
+  devise_for :users, controllers: { registrations: 'users/registrations',
     sessions: 'users/sessions' }
+  resources :users, :only => [:index]
   resources :books
 
   if Rails.env.development?
