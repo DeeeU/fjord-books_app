474dbc57d3fc4068ee746838433b507ebd2e0480
Y.Kanabuchi <yutaro.kanabuchi@medpeer.co.jp>
2022-08-05 09:00:23 +0900

    add: indexページの導入とuserのname,postcode,adderss,introductionの保存を可能に 一覧ページを作成とのことだったのでindexページを新たに追加した。 一覧ページに載せたい、追加予定だったカラムをmigrate後にDB に保存されるようにした。 [参考記事] カラムの追加　https://qiita.com/nowshi/items/a7220036a684a9ac4bb4 追加したカラムの名前変更　https://qiita.com/libertyu/items/93acd8733e34b1d0a63c カラムをDBに登録できるように　https://qiita.com/hz1_d/items/77e440b09a1ddbe56263

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    added_attrs = [:name,:email,:address,:postcode,:introduction]
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+  end
 end
