7a9b1ceb257afb49627519c7a50dbeb98150b5a3
Y.Kanabuchi <yutaro.kanabuchi@medpeer.co.jp>
2022-08-22 18:06:13 +0900

    add: active storage

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,9 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  private
+    def user_params
+      params.require(:user).permit(:password, :avatar)
+    end
 end
