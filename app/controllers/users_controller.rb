7c600087662c97719de32bafa73fdd045034e8e5
Y.Kanabuchi <yutaro.kanabuchi@medpeer.co.jp>
2022-08-26 10:47:02 +0900

    change: 画像ファイル追加の追記 プロフィール画像の追加をした後の表記を追加した 一覧ではアップロードがされていない際の仮画像を添付するようにし、アップロードされている場合はアップロードされている画像を使用sされている画像を使用する 詳細ではアップロードされていなければ表記をせず、アップロード時のみ画像と見出しが見えるようにしている

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,9 +8,4 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
-
-  private
-    def user_params
-      params.require(:user).permit(:password, :avatar)
-    end
 end
