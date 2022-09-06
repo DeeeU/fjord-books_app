a78a69612f87ea6433fd193383e976660611a3ae
Y.Kanabuchi <yutaro.kanabuchi@medpeer.co.jp>
2022-09-06 18:27:33 +0900

    change : crudの追加 Create　大まかは実装完了。URLの入力回避はまだ Read Createで作られたデータが見えるようにはなった。 Update　全ての情報が一気に更新される。ここに更新されるように変更更新されるように変更が必要 Delete　未実装

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,13 +1,44 @@
 class ReportsController < ApplicationController
+  before_action :set_report, only: %i[show edit update destroy]
+
   def index
+    @reports = Report.all
+
   end
 
   def new
+    @report = Report.new
+  end
+
+  def create
+    @report =Report.new(report_params)
+    if @report.save
+      redirect_to reports_path
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    if @report = Report.update(report_params)
+      redirect_to reports_path
+    else
+      render 'edit'
+    end
   end
 
   def edit
   end
 
   def show
+  end
+
+  private
+  def set_report
+    @report = Report.find(params[:id])
+  end
+
+  def report_params
+    params.require(:report).permit(:title, :text)
   end
 end
