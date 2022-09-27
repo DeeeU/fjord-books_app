59c4a691c20893ae825832edc378797cd5a1b2e6
Y.Kanabuchi <yutaro.kanabuchi@medpeer.co.jp>
2022-09-27 13:17:06 +0900

    change : 遷移先をユーザ詳細ページに変更

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -3,11 +3,11 @@
 class FollowsController < ApplicationController
   def create
     current_user.follow(params[:user_id])
-    redirect_to request.referer
+    redirect_to users_path
   end
 
   def destroy
     current_user.unfollow(params[:user_id])
-    redirect_to request.referer
+    redirect_to users_path
   end
 end
