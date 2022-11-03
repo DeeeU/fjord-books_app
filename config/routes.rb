0df3d30b0a58180d9681d64660fed801e7ecdea8
Y.Kanabuchi <yutaro.kanabuchi@medpeer.co.jp>
2022-11-04 08:59:21 +0900

    change : ポリモーフィック関連の追記

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   devise_for :users
   root to: 'books#index'
+  resources :comments
   resources :books
   resources :reports
   resources :users, only: %i[index show] do
