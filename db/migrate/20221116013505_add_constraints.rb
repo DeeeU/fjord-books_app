32d53b5e233e970e5f8af9eb3fd3fbb283545e69
Y.Kanabuchi <yutaro.kanabuchi@medpeer.co.jp>
2022-11-16 11:14:02 +0900

    change : notnull制約の追加

---
+++ db/migrate/20221116013505_add_constraints.rb
@@ -0,0 +1,8 @@
+class AddConstraints < ActiveRecord::Migration[6.1]
+  def change
+    change_column :reports, :title, :text, null: false
+    change_column :reports, :text, :text, null: false
+    change_column :comments, :text, :text, null: false
+    change_column :reports, :created_by, :bigint, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_11_025508) do
+ActiveRecord::Schema.define(version: 2022_11_16_013505) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -53,7 +53,7 @@ ActiveRecord::Schema.define(version: 2022_11_11_025508) do
     t.string "postable_type", null: false
     t.integer "postable_id", null: false
     t.integer "user_id", null: false
-    t.text "text"
+    t.text "text", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["postable_type", "postable_id"], name: "index_comments_on_postable"
@@ -70,8 +70,8 @@ ActiveRecord::Schema.define(version: 2022_11_11_025508) do
   end
 
   create_table "reports", force: :cascade do |t|
-    t.string "title"
-    t.string "text"
+    t.text "title", null: false
+    t.text "text", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "created_by"
