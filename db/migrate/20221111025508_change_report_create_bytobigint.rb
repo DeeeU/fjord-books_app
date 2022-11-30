22ec44daf5a5a8a652af9b0bc08e7d3ed6b15916
Y.Kanabuchi <yutaro.kanabuchi@medpeer.co.jp>
2022-11-30 10:52:44 +0900

    down : reportsテーブルのカラムの方変更の撤廃

--- db/migrate/20221111025508_change_report_create_bytobigint.rb
+++ db/migrate/20221111025508_change_report_create_bytobigint.rb
@@ -1,5 +1,5 @@
 class ChangeReportCreateBytobigint < ActiveRecord::Migration[6.1]
-  def change
+  def up
     change_column :reports, :created_by, 'bigint USING CAST(parent_id AS bigint)'
   end
 end
