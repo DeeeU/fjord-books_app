c27dcc18a57c0ca19f1a41c0b8d2f2b97fec9ab5
Y.Kanabuchi <yutaro.kanabuchi@medpeer.co.jp>
2022-11-30 11:08:05 +0900

    delete: 不要なmigrateファイルの削除

--- db/migrate/20221111025508_change_report_create_bytobigint.rb
+++
@@ -1,5 +0,0 @@
-class ChangeReportCreateBytobigint < ActiveRecord::Migration[6.1]
-  def up
-    change_column :reports, :created_by, 'bigint USING CAST(parent_id AS bigint)'
-  end
-end
--- db/migrate/20221116013505_add_constraints.rb
+++
@@ -1,8 +0,0 @@
-class AddConstraints < ActiveRecord::Migration[6.1]
-  def up
-    change_column :reports, :title, :text, null: false
-    change_column :reports, :text, :text, null: false
-    change_column :comments, :text, :text, null: false
-    change_column :reports, :created_by, :bigint, foreign_key: true
-  end
-end
