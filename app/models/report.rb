7407bdb9ca8a07baaf9607fcb9330de1360c9114
Y.Kanabuchi <yutaro.kanabuchi@medpeer.co.jp>
2022-11-30 11:25:17 +0900

    change: モデルの関連付けの変更

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
 class Report < ApplicationRecord
+  belongs_to :user, dependent: :destroy
   has_many :comments, as: :postable, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,10 @@ class User < ApplicationRecord
 
   has_many :active_relationships, class_name: 'Relationship', foreign_key: :follower_id, dependent: :destroy, inverse_of: :follower
   has_many :followings, through: :active_relationships, source: :following
-
   has_many :passive_relationships, class_name: 'Relationship', foreign_key: :following_id, dependent: :destroy, inverse_of: :following
   has_many :followers, through: :passive_relationships, source: :follower
-  has_many :comments, dependent: :destroy
+  has_many :reports
+  has_many :comments
   has_one_attached :avatar
 
   def following?(user)
