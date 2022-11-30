a2babc7201de2ae5f0665eac7bf969d647a48557
Y.Kanabuchi <yutaro.kanabuchi@medpeer.co.jp>
2022-11-30 13:30:54 +0900

    change: オプションの訂正

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,5 @@
 
 class Comment < ApplicationRecord
   belongs_to :postable, polymorphic: true
-  belongs_to :user, dependent: :destroy
+  belongs_to :user
 end
--- app/models/report.rb
+++ app/models/report.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
 class Report < ApplicationRecord
-  belongs_to :user, dependent: :destroy
   has_many :comments, as: :postable, dependent: :destroy
 end
