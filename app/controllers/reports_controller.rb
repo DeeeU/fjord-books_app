4a9738c36e901e6e4a591f749a0266f61a0d517d
Y.Kanabuchi <yutaro.kanabuchi@medpeer.co.jp>
2022-12-06 13:31:25 +0900

    change: カラム名の変更による修正（created_by）

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -19,7 +19,7 @@ class ReportsController < ApplicationController
 
   def create
     @report = Report.new(report_params)
-    @report.created_by = current_user.id
+    @report.user_id = current_user.id
     respond_to do |format|
       if @report.save
         format.html { redirect_to reports_url, notice: t('controllers.common.notice_create', name: Report.model_name.human) }
