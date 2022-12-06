1f86c07bbe645e216a327ee8d2f604d89537dc66
Y.Kanabuchi <yutaro.kanabuchi@medpeer.co.jp>
2022-12-06 16:58:13 +0900

    change: reportテーブルのカラムの型定義及び制約の変更

--- db/migrate/20221027231155_create_reports.rb
+++ db/migrate/20221027231155_create_reports.rb
@@ -3,7 +3,7 @@ class CreateReports < ActiveRecord::Migration[6.1]
     create_table :reports do |t|
       t.string :title, null: false
       t.string :text, null: false
-      t.bigint :user_id, foreign_key: true
+      t.references :user, null: false, foreign_key: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -72,9 +72,10 @@ ActiveRecord::Schema.define(version: 2022_10_28_014818) do
   create_table "reports", force: :cascade do |t|
     t.string "title", null: false
     t.string "text", null: false
-    t.bigint "user_id"
+    t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_reports_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -96,4 +97,5 @@ ActiveRecord::Schema.define(version: 2022_10_28_014818) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "comments", "users"
+  add_foreign_key "reports", "users"
 end
