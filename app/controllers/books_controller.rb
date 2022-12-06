b07ae5dfcf09863884c5bce22ff7a9bf3333a881
Y.Kanabuchi <yutaro.kanabuchi@medpeer.co.jp>
2022-12-06 16:10:37 +0900

    change: 削除したjsonレスポンスの追記

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -28,12 +28,13 @@ class BooksController < ApplicationController
   # POST /books.json
   def create
     @book = Book.new(book_params)
-
     respond_to do |format|
       if @book.save
         format.html { redirect_to @book, notice: t('controllers.common.notice_create', name: Book.model_name.human) }
+        format.json { render :show, status: :created, location: @book }
       else
         format.html { render :new }
+        format.json { render json: @book.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -44,8 +45,10 @@ class BooksController < ApplicationController
     respond_to do |format|
       if @book.update(book_params)
         format.html { redirect_to @book, notice: t('controllers.common.notice_update', name: Book.model_name.human) }
+        format.json { render :show, status: :ok, location: @book }
       else
         format.html { render :edit }
+        format.json { render json: @book.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -56,6 +59,7 @@ class BooksController < ApplicationController
     @book.destroy
     respond_to do |format|
       format.html { redirect_to books_url, notice: t('controllers.common.notice_destroy', name: Book.model_name.human) }
+      format.json { head :no_content }
     end
   end
 
