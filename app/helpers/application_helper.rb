8e132afc854ad72b9cbef3a4ced842748c649b72
Y.Kanabuchi <yutaro.kanabuchi@medpeer.co.jp>
2023-02-13 13:29:53 +0900

    change: 名前とメールアドレスの表示識別の改善

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def name_or_email(item)
+    if item.user.name.empty?
+      item.user.email
+    else
+      item.user.name
+    end
+  end
 end
