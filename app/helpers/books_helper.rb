74c76c93125f19213cef6d588bbd2caa75668c41
Y.Kanabuchi <yutaro.kanabuchi@medpeer.co.jp>
2023-02-24 11:22:56 +0900

    change : email_or_nameの場所変更

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,11 +1,4 @@
 # frozen_string_literal: true
 
 module BooksHelper
-  def name_or_email(item)
-    if item.user.name.empty?
-      item.user.email
-    else
-      item.user.name
-    end
-  end
 end
