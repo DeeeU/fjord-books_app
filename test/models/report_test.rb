65d424bffb0476962398e55b64ce55f91b6fcd25
Y.Kanabuchi <yutaro.kanabuchi@medpeer.co.jp>
2023-03-09 17:43:31 +0900

    add : 日報のモデルテストの追加

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,15 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "#editable_user_validation?" do
+    user1 = User.create!(name: 'user1', email: 'user1@yhogehoge.com', password: 'password')
+    report1 = Report.new(user_id: user1.id, title: 'report1', content: 'report1' )
+    assert_equal true, report1.editable?(user1)
+  end
+
+  test "#show_date_format" do
+    user2 = User.create!(name: 'user2', email: 'user2@yhogehoge.com', password: 'password')
+    report2 = Report.new(user_id: user2.id, title: 'report2', content: 'report2', created_at: '2000-01-01 00:00:01 UTC')
+    assert_equal(report2.created_at.to_date, report2.created_on )
+  end
 end
