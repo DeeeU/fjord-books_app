818599d61bbc2f6e07be41db2a0ae837d1d597ae
Y.Kanabuchi <yutaro.kanabuchi@medpeer.co.jp>
2023-03-16 16:27:19 +0900

    change: click_onの脱却

--- test/system/books_test.rb
+++ test/system/books_test.rb
@@ -20,41 +20,41 @@ class BooksTest < ApplicationSystemTestCase
 
   test 'creating a Book' do
     visit books_url
-    click_on '新規作成'
+    click_link '新規作成'
 
     fill_in 'タイトル', with: "This is Alice's book."
     fill_in 'メモ', with: "Book Alice"
     fill_in '著者', with: "alice"
 
-    click_on '登録する'
+    click_button '登録する'
 
     assert_text '本が作成されました。'
     assert_text "Book Alice"
 
-    click_on '戻る'
+    click_link '戻る'
     assert_current_path(books_path)
   end
 
   test 'updating a Book' do
     visit books_url
-    click_link('編集')
+    click_link '編集'
 
     fill_in 'タイトル', with: "This is new Alice's book."
     fill_in 'メモ', with: "New Book Alice"
     fill_in '著者', with: "Ms.alice"
-    click_on '更新する'
+    click_button '更新する'
 
     assert_text '本が更新されました。'
     assert_text "This is new Alice's book."
 
-    click_on '戻る'
+    click_link '戻る'
     assert_current_path(books_path)
   end
 
   test 'destroying a Book' do
     visit books_url
     page.accept_confirm do
-      click_link('削除')
+      click_link '削除'
     end
 
     assert_text '本が削除されました。'
