695d140b093d25783e6d704cfe5b7ef3e6b36759
Y.Kanabuchi <yutaro.kanabuchi@medpeer.co.jp>
2023-03-27 10:21:33 +0900

    change : ダブルクォートへの変更

--- test/system/books_test.rb
+++ test/system/books_test.rb
@@ -5,14 +5,14 @@ require 'application_system_test_case'
 class BooksTest < ApplicationSystemTestCase
   setup do
     visit root_url
-    fill_in 'Eメール', with: 'alice@hoge.com'
-    fill_in 'パスワード', with: 'password'
-    click_button 'ログイン'
+    fill_in "Eメール", with: "alice@hoge.com"
+    fill_in "パスワード", with: "password"
+    click_button "ログイン"
   end
 
   test 'visiting the index' do
     visit books_url
-    assert_selector 'h1', text: '本'
+    assert_selector "h1", text: "本"
     assert_text "Book Alice"
     assert_text "alice@hoge.com"
     assert_text "alice"
@@ -20,44 +20,44 @@ class BooksTest < ApplicationSystemTestCase
 
   test 'creating a Book' do
     visit books_url
-    click_link '新規作成'
+    click_link "新規作成"
 
-    fill_in 'タイトル', with: "This is Alice's book."
-    fill_in 'メモ', with: "Book Alice"
-    fill_in '著者', with: "alice"
+    fill_in "タイトル", with: "This is Alice's book."
+    fill_in "メモ", with: "Book Alice"
+    fill_in "著者", with: "alice"
 
-    click_button '登録する'
+    click_button "登録する"
 
-    assert_text '本が作成されました。'
+    assert_text "本が作成されました。"
     assert_text "Book Alice"
 
-    click_link '戻る'
+    click_link "戻る"
     assert_current_path(books_path)
   end
 
   test 'updating a Book' do
     visit books_url
-    click_link '編集'
+    click_link "編集"
 
-    fill_in 'タイトル', with: "This is new Alice's book."
-    fill_in 'メモ', with: "New Book Alice"
-    fill_in '著者', with: "Ms.alice"
-    click_button '更新する'
+    fill_in "タイトル", with: "This is new Alice's book."
+    fill_in "メモ", with: "New Book Alice"
+    fill_in "著者", with: "Ms.alice"
+    click_button "更新する"
 
-    assert_text '本が更新されました。'
+    assert_text "本が更新されました。"
     assert_text "This is new Alice's book."
 
-    click_link '戻る'
+    click_link "戻る"
     assert_current_path(books_path)
   end
 
   test 'destroying a Book' do
     visit books_url
     page.accept_confirm do
-      click_link '削除'
+      click_link "削除"
     end
 
-    assert_text '本が削除されました。'
+    assert_text "本が削除されました。"
     assert_no_text "This is new Alice's book."
 
     assert_current_path(books_path)
--- test/system/reports_test.rb
+++ test/system/reports_test.rb
@@ -5,14 +5,14 @@ require 'application_system_test_case'
 class ReportsTest < ApplicationSystemTestCase
   setup do
     visit root_url
-    fill_in 'Eメール', with: 'alice@hoge.com'
-    fill_in 'パスワード', with: 'password'
-    click_button 'ログイン'
+    fill_in "Eメール", with: "alice@hoge.com"
+    fill_in "パスワード", with: "password"
+    click_button "ログイン"
   end
 
   test 'visiting the Report index' do
     visit reports_url
-    assert_selector 'h1', text: '日報'
+    assert_selector "h1", text: "日報"
     assert_text "Report Alice"
     assert_text "alice@hoge.com"
     assert_text Date.today.strftime('%Y/%m/%d')
@@ -20,43 +20,43 @@ class ReportsTest < ApplicationSystemTestCase
 
   test 'creating the Report' do
     visit reports_url
-    click_link '新規作成'
+    click_link "新規作成"
 
-    fill_in 'タイトル', with: "This is Alice's report."
-    fill_in '内容', with: "Report Alice"
+    fill_in "タイトル", with: "This is Alice's report."
+    fill_in "内容", with: "Report Alice"
 
-    click_button '登録する'
+    click_button "登録する"
 
-    assert_text '日報が作成されました。'
+    assert_text "日報が作成されました。"
     assert_text "Report Alice"
 
-    click_link '戻る'
+    click_link "戻る"
     assert_current_path(reports_path)
 
   end
 
   test 'editing the Report' do
     visit reports_url
-    click_link '編集'
+    click_link "編集"
 
-    fill_in 'タイトル', with: "This is new Alice's  report."
-    fill_in '内容', with: "New Report Alice"
-    click_button '更新する'
+    fill_in "タイトル", with: "This is new Alice's  report."
+    fill_in "内容", with: "New Report Alice"
+    click_button "更新する"
 
-    assert_text '日報が更新されました。'
+    assert_text "日報が更新されました。"
     assert_text "This is new Alice's report."
 
-    click_link '戻る'
+    click_link "戻る"
     assert_current_path(reports_path)
   end
 
   test 'destroying the Report' do
     visit reports_url
     page.accept_confirm do
-      click_link '削除'
+      click_link "削除"
     end
 
-    assert_text '日報が削除されました。'
+    assert_text "日報が削除されました。"
     assert_no_text "This is new Alice's report."
 
     assert_current_path(reports_path)
